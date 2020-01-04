28e1c9a03fef7a3c7b56a0ccb87f520069a0c98a
Phillbot <d.philippenko@gmail.com>
2020-01-04 12:27:27 +0200

    Updates

---
+++ precache-manifest.7058d90665e707e64f8135746600515f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f056fa6723c7146c99d8412e01b45fe",
+    "url": "/yalantis-react-school/index.html"
+  },
+  {
+    "revision": "3ff728ded97988dcf910",
+    "url": "/yalantis-react-school/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "801c9ae148975ae221fc",
+    "url": "/yalantis-react-school/static/css/main.b5ef0568.chunk.css"
+  },
+  {
+    "revision": "3ff728ded97988dcf910",
+    "url": "/yalantis-react-school/static/js/2.096158da.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/yalantis-react-school/static/js/2.096158da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "801c9ae148975ae221fc",
+    "url": "/yalantis-react-school/static/js/main.070623b4.chunk.js"
+  },
+  {
+    "revision": "56a26f4e888604122ff2",
+    "url": "/yalantis-react-school/static/js/runtime-main.0673064b.js"
+  }
+]);
